01dc8af5e164fa51eaa63c473e4d9d8337883f6a
Manob Biswas <manob7@hotmail.com>
2018-02-14 12:06:14 +0600

    init commit

---
+++ app/src/main/java/com/appsgang/mbrowser/Browser.java
@@ -0,0 +1,13 @@
+package com.appsgang.mbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Browser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browser);
+    }
+}
